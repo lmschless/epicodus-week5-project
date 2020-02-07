ff9bccccfff58734cde743bffcf967887c870d0c
Luke <lmschless@gmail.com>
2020-02-06 17:09:49 -0800

    finished the majority of the looping branching logic.

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 $(document).ready(function() {
 	$('#formOne').submit(function() {
+		event.preventDefault();
 		//console.log(arr);
 		const beepBoop = () => {
 			const input = $('#userString').val();
-			const splitInput = input.split('');
-			console.log(splitInput);
+			// console.log(splitInput);
 			if (isNaN(input)) {
 				console.log('Error: please input a number!');
 				// return;
 			}
 			let currentNumber = 0;
-			// while (currentNumber !== parseInt(input)) {
-			// 	currentNumber += 1;
-			// 	// 	if (currentNumber ===  console.log(currentNumber);
-			// 	// }
-			// 	return;
-			// }
-			event.preventDefault();
+
+			while (currentNumber !== parseInt(input)) {
+				let currentSplit = currentNumber.toString().split('');
+				// console.log(currentNumber);
+				// console.log(currentSplit);
+				if (currentSplit.includes('3')) {
+					$('#');
+					console.log("I'm sorry, Dave. I'm afraid I can't do that.");
+				} else if (currentSplit.includes('2')) {
+					console.log('Boop!');
+				} else if (currentSplit.includes('1')) {
+					console.log('Beep!');
+				} else {
+					console.log(currentNumber);
+				}
+				currentNumber += 1;
+
+				// 	if (currentNumber ===  console.log(currentNumber);
+				// }
+				// }
+			}
 		};
 		beepBoop();
 	});
