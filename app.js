b40e9ab323cae76692799db83cf21fe9a8f73b23
Luke <lmschless@gmail.com>
2020-02-07 10:23:54 -0800

    adjusted layout of error image and CSS rules.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
 		if (isNaN(input)) {
 			$('#summary').append(
-				`<h1>Error does not compute: please enter a number.</h1><div class= "container vw-10 vh-10"><img src="https://images.unsplash.com/photo-1546776310-eef45dd6d63c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&" class="img-thumbnail vw-10 vh-10"></div>`
+				`<div id="img-div"><h2>Error does not compute: please enter a number.</h2><img src="https://images.unsplash.com/photo-1546776310-eef45dd6d63c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&" class=""></div>`
 			);
 			return;
 		}
