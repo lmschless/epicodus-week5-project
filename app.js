5e20d9b69cfdbf9adfdbb4221f3e810a4e04c2dd
Luke <lmschless@gmail.com>
2020-02-06 16:28:28 -0800

    starting loop logic

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
 	$('#formOne').submit(function() {
-		const input = $('#userString').val();
 		//console.log(arr);
+		const beepBoop = () => {
+			const input = $('#userString').val();
+			if (isNaN(input)) {
+				console.log('Error: please input a number!');
+        return; 
+      }
+        if ()
+      }
+       
+		};
+
 		event.preventDefault();
 	});
 });
